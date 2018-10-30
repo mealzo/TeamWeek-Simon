8010eb45a6fedbb23ca7a540742161b4011b189f
Student <example@example.com>
2018-10-29 19:46:06 -0700

    add the function to record button clicks and put the values of each button into a array called userArray

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+//continuous values for each button
+var redButton = 1;
+var blueButton = 2;
+var greenButton = 3;
+var yellowButton = 4;
+
+///array of user presses
+var userArray = [];
+//function to clear userArray
+var clear = function(){
+
+}
+
+//how many times each button is clicked
+var redPress = 0;
+var bluePress = 0;
+var greenPress = 0;
+var yellowPress = 0;
+
+
+$(document).ready(function(){
+  $("#red").click(function(){
+    userArray.push(redButton);
+     redPress += 1;
+     console.log(userArray);
+     //console.log(redPress);
+  });
+  $("#blue").click(function(){
+    userArray.push(blueButton);
+     bluePress += 1;
+     console.log(userArray);
+     //console.log(bluePress);
+  });
+  $("#green").click(function(){
+    userArray.push(greenButton);
+     greenPress += 1;
+     console.log(userArray);
+     //console.log(greenPress);
+  });
+  $("#yellow").click(function(){
+    userArray.push(yellowButton);
+     yellowPress += 1;
+     console.log(userArray);
+     //console.log(yellowPress);
+
+  });
+ 
+});
